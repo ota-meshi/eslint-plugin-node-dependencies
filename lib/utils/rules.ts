3cd14d56bf0c70f91a9ce40cbad3856a0b08c202
Yosuke Ota <otameshiyo23@gmail.com>
2022-03-17 04:50:52 +0000

    update

--- lib/utils/rules.ts
+++ lib/utils/rules.ts
@@ -2,8 +2,8 @@ import type { RuleModule } from "../types"
 import absoluteVersion from "../rules/absolute-version"
 import compatEngines from "../rules/compat-engines"
 import noDeprecated from "../rules/no-deprecated"
-import noRestrictedDeps from "../rules/no-restricted-deps"
 import noDupeDeps from "../rules/no-dupe-deps"
+import noRestrictedDeps from "../rules/no-restricted-deps"
 import validEngines from "../rules/valid-engines"
 import validSemver from "../rules/valid-semver"
 
@@ -11,8 +11,8 @@ export const rules = [
     absoluteVersion,
     compatEngines,
     noDeprecated,
-    noRestrictedDeps,
     noDupeDeps,
+    noRestrictedDeps,
     validEngines,
     validSemver,
 ] as RuleModule[]
