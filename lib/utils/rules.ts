4893e7b89bef9d34e8b0daf0bf3198c1553b5c5f
Yosuke Ota <otameshiyo23@gmail.com>
2022-03-17 13:47:24 +0900

    Add no-dupe-deps rule (#36)
    * Add no-dupe-deps rule
    
    * Update no-dupe-deps.ts

--- lib/utils/rules.ts
+++ lib/utils/rules.ts
@@ -2,6 +2,7 @@ import type { RuleModule } from "../types"
 import absoluteVersion from "../rules/absolute-version"
 import compatEngines from "../rules/compat-engines"
 import noDeprecated from "../rules/no-deprecated"
+import noDupeDeps from "../rules/no-dupe-deps"
 import validEngines from "../rules/valid-engines"
 import validSemver from "../rules/valid-semver"
 
@@ -9,6 +10,7 @@ export const rules = [
     absoluteVersion,
     compatEngines,
     noDeprecated,
+    noDupeDeps,
     validEngines,
     validSemver,
 ] as RuleModule[]
