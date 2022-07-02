c887451e95321fe7b05a3b01bd8e0cb6177f8595
yosuke ota <otameshiyo23@gmail.com>
2022-07-02 17:53:31 +0900

    Improve performance

---
+++ lib/utils/package-json/index.ts
@@ -0,0 +1,10 @@
+import { createSyncFn } from "synckit"
+import type { AbbreviatedMetadata, Options } from "package-json"
+type SyncPackageJson = (
+    packageName: string,
+    options: Options,
+) => AbbreviatedMetadata
+
+export const syncPackageJson: SyncPackageJson = createSyncFn(
+    require.resolve("./worker"),
+)
