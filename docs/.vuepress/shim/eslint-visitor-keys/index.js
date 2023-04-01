7d41012d73cfc15f7ba9d00408247fe7e494460c
yosuke ota <otameshiyo23@gmail.com>
2023-04-01 19:28:17 +0900

    chore: format

--- docs/.vuepress/shim/eslint-visitor-keys/index.js
+++ docs/.vuepress/shim/eslint-visitor-keys/index.js
@@ -2,6 +2,6 @@
 /* eslint-disable node/no-unsupported-features/es-syntax -- DEMO */
 import * as all from "../../../../node_modules/eslint-visitor-keys/lib/index";
 
-const evk = all.default || all
+const evk = all.default || all;
 export const { KEYS, getKeys, unionWith } = evk;
 export default evk;
