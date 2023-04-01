781f4e28c53e57cd3e54b299e0bf0a0d9ca38b3b
yosuke ota <otameshiyo23@gmail.com>
2023-04-01 17:39:37 +0900

    chore: fix doc site script

---
+++ docs/.vuepress/shim/eslint-visitor-keys/index.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair -- DEMO
+/* eslint-disable node/no-unsupported-features/es-syntax -- DEMO */
+import * as all from "../../../../node_modules/eslint-visitor-keys/lib/index";
+
+const evk = all.default || all
+export const { KEYS, getKeys, unionWith } = evk;
+export default evk;
