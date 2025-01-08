6314565372bdea4c65b30517ce337afb8e1700d6
Marcus R. Brown <git@mrbro.dev>
2025-01-07 21:47:41 -0700

    chore: replace uses of `ts-node` with `tsx`
    Unlike ts-node, tsx supports ESM syntax in CJS output without throwing errors.

--- tools/new-rule.ts
+++ tools/new-rule.ts
@@ -144,7 +144,7 @@ This rule reports ???.
   // eslint-disable-next-line no-console -- ignore
   console.log(`Test Command:
 
-${yellow}npx mocha --require ts-node/register/transpile-only "tests/**/${ruleId}.ts" --reporter dot --timeout 60000${reset}
+${yellow}npx mocha --require tsx/cjs "tests/**/${ruleId}.ts" --reporter dot --timeout 60000${reset}
 
 `);
 })(process.argv[2]);
