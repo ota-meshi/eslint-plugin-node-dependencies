31af9cb0e4b012fc0da67c8de624784b9413489f
yosuke ota <otameshiyo23@gmail.com>
2026-02-21 09:36:17 +0900

    fix

--- lib/index.ts
+++ lib/index.ts
@@ -2,10 +2,11 @@ import type { RuleModule } from "./types";
 import { rules as ruleList } from "./utils/rules";
 import { recommendedConfig as flatRecommended } from "./configs/flat/recommended";
 import * as meta from "./meta";
+import type { Linter } from "eslint";
 
 const configs = {
-  recommended: flatRecommended,
-  "flat/recommended": flatRecommended,
+  recommended: flatRecommended as Linter.Config[],
+  "flat/recommended": flatRecommended as Linter.Config[],
 };
 
 const rules = ruleList.reduce(
